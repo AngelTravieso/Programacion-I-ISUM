0942cc8928dd49248e690acfeb150aa27d5f8b15
AngelTravieso <angeltraviesoc@gmail.com>
2023-10-01 13:38:52 -0400

    UPDATE - 01-sintaxis-basica.cpp

--- C++/01-sintaxis-basica.cpp
+++ C++/01-sintaxis-basica.cpp
@@ -1,11 +1,15 @@
+// El lenguaje C++ define varios encabezados, que contienen información necesaria o útil para su programa. Para este programa, se necesita el encabezado <iostream>.
 #include <iostream>
 
+// La línea que usa el espacio de nombres std; le dice al compilador que use el espacio de nombres estándar. Los espacios de nombres son una adición relativamente reciente a C++.
 using namespace std;
 
+// main() es donde la ejecución del programa comienza
 int main() {
 
-    cout << "Hola Mundo desde C++" << endl;
+    cout << "Hola Mundo desde C++" << endl; // Imprime "Hola Mundo desde C++"
 
+    // La siguiente línea devuelve 0; finaliza la función principal () y hace que devuelva el valor 0 al proceso de llamada.
     return 0;
 }
 
