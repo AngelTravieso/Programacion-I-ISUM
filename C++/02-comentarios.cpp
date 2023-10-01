7c20e5d5eb2dbb47da3dd0de25f401e8804200dd
AngelTravieso <angeltraviesoc@gmail.com>
2023-10-01 13:52:40 -0400

    ADD - 02-comentarios.cpp

---
+++ C++/02-comentarios.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    /* Esto es un comentario */
+
+    /*
+       C++ también soporta comentarios
+       multilineas
+    */
+
+   // También puede empezar con //
+
+   cout << "Hola Mundo"; // Imprime "Hola Mundo"
+
+    return 0;
+
+}
