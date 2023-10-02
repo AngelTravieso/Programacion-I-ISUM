2c8f0f5a5357c1d555b51cfb4804a8c296b6cf7f
AngelTravieso <angeltraviesoc@gmail.com>
2023-10-01 23:07:54 -0400

    ADD - 04-tipos-variables.cpp

---
+++ C++/04-tipos-variables.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+extern int a, b;
+extern int c;
+extern float f;
+
+int main() {
+
+    // Declaración de variables
+    int a, b;
+    int c;
+    float f;
+
+    // Inicialización de variables
+    a = 10;
+    b = 20;
+    c = a + b;
+
+    cout << c << endl;
+
+    f = 70.0/3.0;
+    cout << f << endl;
+
+    return 0;
+
+}
