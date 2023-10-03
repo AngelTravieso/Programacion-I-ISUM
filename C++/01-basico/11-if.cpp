c711ad6b1a7158914345604a24517215d5a35370
AngelTravieso <angeltraviesoc@gmail.com>
2023-10-03 16:12:05 -0400

    ADD - 11-if.cpp

---
+++ C++/01-basico/11-if.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // Declaración variable local
+   int a = 10;
+ 
+   // Evaluar la condición booleana
+   if( a < 20 ) {
+      // Si la condición se cumple (es verdadero) imprime lo siguiente
+      cout << "a es menor a 20;" << endl;
+   }
+   cout << "El valor de a es : " << a << endl;
+ 
+   return 0;
+
+}
