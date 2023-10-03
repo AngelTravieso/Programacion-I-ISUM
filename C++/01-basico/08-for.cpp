0e410d3aa0a45592b04ba3c0b7bba68e232e94a6
AngelTravieso <angeltraviesoc@gmail.com>
2023-10-03 15:49:50 -0400

    ADD - 08-for.cpp

---
+++ C++/01-basico/08-for.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Un bucle for es una estructura de control de repetición que le permite escribir de manera eficiente un bucle que debe ejecutarse una cantidad específica de veces.
+ * 
+ * La sintaxis de un for en c++ es consta de 3 partes:
+ *          (1)             (2)                (3)
+ * for (inicializacion; condicion; incrementador/decrementador) {
+ *      cuerpo del bucle
+ * }
+*/
+
+int main() {
+    
+    // Ejecución del loop
+    for ( int i = 10; i <= 20; i++) {
+        cout << "El valor de i es: " << i << endl;
+    }
+
+    return 0;
+
+}
